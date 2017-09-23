c9c3a404d7049cf6032bd221b87059b75fc8398c
Cody Cuellar <cody.cuellar@gmail.com>
2017-09-23 15:06:25 -0700

    Added bill splitter script

---
+++ bill_tracker/bin/split_json_bills.py
@@ -0,0 +1,15 @@
+import os
+import json
+from bill_tracker import core
+
+core.load_json()
+
+os.chdir(os.path.dirname(__file__))
+os.chdir('../../data/bills')
+
+
+for bill in core.current_bills:
+    filepath = os.path.join(os.getcwd(), bill.name + '.json')
+    j = bill.to_json()
+    with open(filepath, 'w') as f:
+        json.dump(j, f, indent=4, sort_keys=True)
